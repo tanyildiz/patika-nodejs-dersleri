f043680da5149732b8dd52a6228ea3de817c773b
tanyildiz <tanyildizn@gmail.com>
2025-09-17 23:28:46 +0300

    clean-blog project is finished

--- clean-blog/app.js
+++ clean-blog/app.js
@@ -1,18 +1,30 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description"
-}
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.send(blog)
-})
+    res.render('index', req.query)
+});
+
+app.get("/about", (req, res) => {
+    res.render('about', req.query)
+});
+
+app.get("/add_post", (req, res) => {
+    res.render('add_post', req.query)
+});
+
+app.get("/post", (req, res) => {
+    res.render('post', req.query)
+});
 
 app.listen(port, () => {
     console.log("Server has just started")
-})
+});
